7ce451165d9250b8f3ff79196f2149341a11bff6
Christina Lewis <clewis13@ufl.edu>
2022-06-09 13:25:58 -0400

    button element contains user input

--- index.js
+++ index.js
@@ -2,34 +2,32 @@
 const form = document.querySelector('form')
 
 form.onsubmit = function(e) {
-    //prevent form submission
+    //prevent automatic form submission
     e.preventDefault()
     //access form data by targeting the input
     var ul = document.querySelector('input')
     
     console.log(ul.value)
-    //resets box after user adds item to list- passes test
+
     //insert user input inside #ultodo-list as another button element
     
     var list = document.getElementById('todo-list');
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(ul));
     ul.appendChild(li);
+   
     let createBTN = document.createElement("button");
-    createBTN.innerHTML= "test";
+    createBTN.innerHTML= (ul.value);
     createBTN.type = "submit";
     createBTN.name = "testBTN";
     list.appendChild(createBTN);
+    //resets box after user adds item to list- passes test
     ul.value = ""
     
-    //var listItem = document.getElementById('todo-list')
-    //listItem.innerText = 
+     
 }
 
-//attach this functionality to a form submit event
-//create another click event when user clicks on the to-do list item
-//when user clicks, change text-decoration to strike-through
-//button.onClick = function (e) {
-    //button.style = 'text-decoration: line-through'
-//}
+
+//create another click event when user clicks on the to-do list item for a second time (when user clicks, change text-decoration to strike-through)
+
 //create a 3rd click event that deletes the item from the list after it's been checked off
