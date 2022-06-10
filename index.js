b83c3adc9b6a428497db46c76b774ade68e0bfcc
Christina Lewis <clewis13@ufl.edu>
2022-06-09 20:21:34 -0400

    remove item when clicking twice

--- index.js
+++ index.js
@@ -15,31 +15,23 @@ form.onsubmit = function(e) {
     var ul = document.getElementById('todo-list');
     var newButton = document.createElement('button');
         console.log(userInput.value)
+
     // Create li element within the ul to-do list
     var newLi = document.createElement('li');
     ul.appendChild(newLi);
     newLi.appendChild(newButton);
     newButton.textContent = (userInput.value);
 
+    //mark to-do as done by strike-through
     newButton.onclick = function() {
-    newButton.style = 'text-decoration: line-through'
+    newButton.style = 'text-decoration: line-through';
+
+    //remove to-do when clicking twice
+    newButton.onclick = function() {
+    newButton.remove();
     }
-   
-    //insert user input inside #ultodo-list as another button element
-    /*
-    var list = document.getElementById('todo-list');
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(ul));
-    ul.appendChild(li);
-   
-    let createBTN = document.createElement("button");
-    createBTN.innerHTML= (ul.value);
-    createBTN.type = "submit";
-    createBTN.name = "testBTN";
-    list.appendChild(createBTN);
-    //resets box after user adds item to list- passes test*/
-    //var listItem = document.querySelectorAll('newLi > newButton')
-    //console.log(listItem)
+}
+  
     
     userInput.value = "";    
 }
